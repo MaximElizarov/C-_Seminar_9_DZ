3523713920bb3923b83ce12eee0a97814f15efd3
Maxim Elizmarov <kiro.dota322@gmail.com>
2022-12-22 14:11:22 +0300

    ADD DZ

---
+++ Zadahya_66/Program.cs
@@ -0,0 +1,29 @@
+﻿
+void rec(int m, int n, int sum)
+{
+    if (m > n) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (m++);
+    rec(m, n, sum);
+}
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+rec(m, n, 0);
+
+
+
+
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
---
+++ Zadahya_68/Program.cs
@@ -0,0 +1,24 @@
+﻿
+int Acker (int m, int n)
+{
+    if (m == 0) 
+    return n + 1;
+    if (m != 0 && n == 0) 
+    return Acker(m - 1, 1);
+    if (m > 0 && n > 0) 
+    return Acker(m - 1, Acker(m, n - 1));
+    return Acker(m, n);
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Функция Аккермана для чисел A({m},{n}) = {Acker(m, n)}");
+
+// Денис, спасибо за семинары, было очень позновательно!!!!!!!
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
